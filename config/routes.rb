6f5f8b8eb7ab5950fa595f40409ed6fc728292cb
SilverLongjohns <jakehollinger@hotmail.co.uk>
2020-08-31 19:36:19 +0100

    basic posting functionality added

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'home/index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :posts do
+    resources :comments
+  end
+
+  root 'home#index'
+end
