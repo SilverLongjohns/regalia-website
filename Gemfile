808cfa19bf7e75ef75ed4a42b273ca7693e9d2f1
SilverLongjohns <jakehollinger@hotmail.co.uk>
2020-09-01 20:40:34 +0100

    sidebars and navigation added. Css changes.

--- Gemfile
+++ Gemfile
@@ -24,6 +24,8 @@ gem 'jbuilder', '~> 2.7'
 
 gem 'devise'
 
+gem "simple_calendar", "~> 2.0"
+
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,15 @@
 class User < ApplicationRecord
+  attr_writer :login
+
   has_many :posts
   has_many :comments
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  def login
+    @login || self.username || self.email
+  end
+
 end
